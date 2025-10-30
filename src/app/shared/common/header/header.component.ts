95653533d4d887f1ccdf3eb38305104d8730153d
agustn134 <agustinlopezparra13@gmail.com>
2025-10-30 00:44:49 -0600

    Cambios de Rediseño en el header

--- src/app/shared/common/header/header.component.ts
+++ src/app/shared/common/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-header',
@@ -6,4 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent { }
+export class HeaderComponent implements AfterViewInit { 
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    // 1. Obtener todos los elementos con la clase 'dropdown-toggle'
+    const dropdownToggleEl = document.querySelectorAll('.dropdown-toggle');
+    
+    // 2. Iterar sobre ellos e inicializar el componente Dropdown de Bootstrap
+    dropdownToggleEl.forEach((element: Element) => {
+        // La variable 'bootstrap' está disponible gracias a la declaración 'declare var bootstrap: any;'
+        // Se inicializa cada Dropdown.
+        new bootstrap.Dropdown(element); 
+    });
+
+  }
+}
