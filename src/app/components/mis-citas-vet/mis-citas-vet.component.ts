a05788f95b4fd396a47cee1303e7c74584c3a171
elmanee <cp457920@gmail.com>
2025-10-31 12:29:18 -0600

    login para vet

---
+++ src/app/components/mis-citas-vet/mis-citas-vet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-citas-vet',
+  imports: [],
+  templateUrl: './mis-citas-vet.component.html',
+  styleUrl: './mis-citas-vet.component.scss'
+})
+export class MisCitasVetComponent {
+
+}
--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
 import { AuthService } from '../app/services/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (this.authService.isLoggedIn()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
+  canActivate(route: ActivatedRouteSnapshot): boolean | UrlTree {
+    const token = this.authService.getToken();
+    const rol = this.authService.getUserRole() || ''; // 👈 tu método correcto
+    const allowedRoles = route.data['roles'] as string[];
+
+    if (!token) {
+      console.warn('⛔ No hay token, redirigiendo al login');
+      return this.router.createUrlTree(['/login']);
     }
+
+    if (allowedRoles && !allowedRoles.includes(rol)) {
+      console.warn(`🚫 Acceso denegado: rol "${rol}" no permitido para esta ruta`);
+      if (rol === 'Veterinario') return this.router.createUrlTree(['/veterinario']);
+      if (rol === 'Admin') return this.router.createUrlTree(['/admin']);
+      return this.router.createUrlTree(['/login']);
+    }
+
+    return true;
   }
 }
