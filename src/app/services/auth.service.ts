5d72dac378be7b8f207e7c6af89a96b85ac1cf6d
agustn134 <agustinlopezparra13@gmail.com>
2025-12-01 14:30:22 -0600

    Correo Configuracion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,7 +8,7 @@ import { of } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api/auth';
+  private apiUrl = 'https://vetback-api.onrender.com/api/auth';
 
   constructor(private http: HttpClient, private router: Router) {}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
+// export const environment = {
 
+// };
+export const environment = {
+  production: true,
+  apiUrl: 'https://vetback-api.onrender.com/api' 
 };
