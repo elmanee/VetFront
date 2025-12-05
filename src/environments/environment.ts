25b3dd3276445f01227486e9ba0579b4b421e7ec
elmanee <cp457920@gmail.com>
2025-12-05 10:44:09 -0600

    correcciones para citras desplegado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   // Es la URL de la segunda capa de la arquitectura.
   production: false,
-   apiUrlDos: 'http://localhost:3000', // URL base para todos los endpoints (ej: /api/citas)
-   apiUrl: 'http://localhost:3000/api'
-  //apiUrlDos: 'https://vetback-api.onrender.com',
- // apiUrl: 'https://vetback-api.onrender.com/api'
+  //  apiUrlDos: 'http://localhost:3000', // URL base para todos los endpoints (ej: /api/citas)
+  //  apiUrl: 'http://localhost:3000/api'
+  apiUrlDos: 'https://vetback-api.onrender.com',
+  apiUrl: 'https://vetback-api.onrender.com/api'
 };
 
